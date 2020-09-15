c46e5cd8ebfa61be711fe82396b5ee43b2c1407e
Carlos Roberto <carlosroberto15755@gmail.com>
2020-09-15 13:22:54 -0300

    Migrations, Tabelas, Relations ManyToMany

---
+++ src/shared/infra/typeorm/migrations/1600127756152-AddProductIdToOrdersProducts.ts
@@ -0,0 +1,42 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AddProductIdToOrdersProducts1600127756152
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+
+    await queryRunner.addColumn(
+      'orders_products',
+      new TableColumn({
+        name: 'product_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'orders_products',
+      new TableForeignKey({
+        name: 'ordersProductsProduct',
+        columnNames: ['product_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'products',
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey(
+      'orders_products',
+      'OrdersProductsProduct',
+    );
+
+    await queryRunner.dropColumn('orders_products', 'product_id');
+  }
+}
